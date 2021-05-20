2d65b5b65665eef8844f013e7140df7b0aff82da
Svetlana Petrosyan <petrosyansvetlana7@gmail.com>
2021-05-20 11:04:00 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+ 	$('header .top-part .user-info .dropdown.goroxxx .clickable-heading').on('click' , function() {
+ 		$(this).closest('.dropdown.goroxxx').toggleClass('open')
+ 	})
+})
